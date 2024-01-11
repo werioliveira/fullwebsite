ee85fd45868ba0f28123101640d1e42587d63207
werioliveira <werioliveira@hotmail.com>
2024-01-11 10:36:20 -0300

    fix imports

--- app/(main)/profile/page.jsx
+++ app/(main)/profile/page.jsx
@@ -1,17 +1,15 @@
-import { getServerSession } from "next-auth";
 import React from "react";
-import { authOptions } from "../api/auth/[...nextauth]/route";
-import { redirect } from "next/navigation";
-import ProfileForm from "../components/ProfileForm";
+
+import ProfileForm from "../../components/ProfileForm";
 
 const Page = async () => {
-  //const session = await getServerSession(authOptions);
-  //if (!session) redirect("/signin?callbackUrl=/profile");
-  return (
-    <div>
-      <ProfileForm />
-    </div>
-  );
+	//const session = await getServerSession(authOptions);
+	//if (!session) redirect("/signin?callbackUrl=/profile");
+	return (
+		<div>
+			<ProfileForm />
+		</div>
+	);
 };
 
 export default Page;
